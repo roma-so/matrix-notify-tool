33831ff774633351730176799fee6393c11da1b2
roma-so <roma@roma.so>
2019-10-10 13:34:06 +0300

    Add page for webhooks

--- index.js
+++ index.js
@@ -5,10 +5,14 @@ const path = require('path')
 
 const PORT = process.env.PORT || 5000
 
+const getGithubWebhook = async (req, res) => {}
+
 // Run express and output last commits
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
+  .post('/github', (req, res) => getGithubWebhook(req, res))
+  .get('/github', (req, res) => res.render('pages/github'))
   .get('/', (req, res) => res.render('pages/index'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
